ad49e254bfda5ba2c06e6e4742a317a97e51b0c6
Chrysolite Nocon <chrysolite.nocon@canstar.com.au>
2023-06-09 12:25:23 +0800

    Yso | Added a caret icon

--- components/ratings/important-notes/important-notes.tsx
+++ components/ratings/important-notes/important-notes.tsx
@@ -1,28 +1,48 @@
-"use client";
-import "./important-notes.css";
+'use client';
+import './important-notes.css';
 import { Block } from "@/models/Story.model";
-import { renderRichText } from "@storyblok/react/rsc";
-import { useState } from "react";
+import { storyblokEditable, renderRichText } from "@storyblok/react/rsc";
+import { useState } from 'react';
 
 const SRImportantNotes = ({ blok }: { blok: Block }) => {
-  const showText = "Show Important Notes*†";
-  const hideText = "Hide Important Notes*†";
+  const showContent = {
+    text: "Show Important Notes*†",
+    icon: "https://a.storyblok.com/f/233862/9x5/681dce0f9c/caret-down.png",
+  }
+
+  const hideContent = {
+    text: "Hide Important Notes*†",
+    icon: "https://a.storyblok.com/f/233862/9x5/26e9846036/caret-up.png",
+  }
 
   const [isShowing, setIsShowing] = useState(false);
-  const [noteTitle, setTitle] = useState(showText);
+  const [noteTitle, setTitle] = useState(showContent.text);
+  const [noteIcon, setIcon] = useState(showContent.icon);
+
 
   const toggle = () => {
     setIsShowing(!isShowing);
-    !isShowing ? setTitle(hideText) : setTitle(showText);
-  };
 
+    let text = showContent.text;
+    let icon = showContent.icon;
+
+    !isShowing ? (text = hideContent.text, icon = hideContent.icon) : null;
+    setTitle(text);
+    setIcon(icon);
+
+  };
+  
   return (
-    <div className="sr-note-container">
-      <button className="sr-note-button" onClick={toggle} type="button">
-        <p>{noteTitle}</p>
+    <div {...storyblokEditable(blok)} className="sr-note-container">
+      <button className='sr-note-button'
+        onClick={toggle}
+        type="button"
+      >
+        <p className="px-3 py-2 flex text-sm">{noteTitle}</p>
+        <img className="sr-caret-icon" src={noteIcon} alt="caret-icon" />
       </button>
-      <div
-        className="p-3"
+
+      <div className='p-3'
         style={{ display: isShowing ? "block" : "none" }}
         dangerouslySetInnerHTML={{
           __html: blok?.notes && convertRichtextToHtml(blok.notes),
