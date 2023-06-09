250a5f1aa838ce4a8967180c8aa199baa7125dff
Riccardo Carzaniga <riccardo.carzaniga@canstar.com.au>
2023-06-09 12:50:28 +1000

    Updated mock API

--- app/api/mock/route.tsx
+++ app/api/mock/route.tsx
@@ -1,9 +1,27 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import data from "./sample";
 
 // http://localhost:3000/api/mock
 
-export const GET = (req: NextApiRequest, res: NextApiResponse) => {
-  return NextResponse.json(data);
+const handler = async (req: NextRequest, res: NextResponse) => {
+  console.log(`Received req with ${req.method} method`);
+
+  if (req.method === "GET") {
+    return NextResponse.json(data[0]);
+  }
+
+  if (req.method === "POST") {
+    const { vertical } = await req.json();
+    console.log(`...having vertical: ${vertical}`);
+    const slice = data.find((entry) => entry.category === vertical);
+    return NextResponse.json(
+      slice || { msg: `No product found for vertical: ${vertical}` }
+    );
+  }
+
+  return NextResponse.json({
+    msg: `Received unexpected method: ${req.method}`,
+  });
 };
+
+export { handler as GET, handler as POST };
